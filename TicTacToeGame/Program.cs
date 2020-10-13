881f8c714849ed08faaf8d19584869e468fcd2b5
Daneesh <mustafadaneesh@gmail.com>
2020-10-13 10:03:29 +0530

    [Daneesh] Refactor : Initilizing Board

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[] Arr = Board();
+        }
+        static char[] Board()
+        {
+            char[] Arr = new char[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Arr[i] = ' ';
+            }
+            return Arr;
+        }
+    }
+}
