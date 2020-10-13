33c2ad2747b717d4cd59ef3db2fa70c8ba0e84c2
Daneesh <mustafadaneesh@gmail.com>
2020-10-13 11:30:20 +0530

    [Daneesh] Refactor : Displaying the TicTacToe Board

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -15,8 +15,9 @@ namespace TicTacToe
                 Comp = 'X';
             Console.WriteLine("Player chose " + Player);
             Console.WriteLine("Computer chose " + Comp);
+            DisplayBoard(Arr);
         }
-        private static char[] Board()
+        public static char[] Board()
         {
             char[] Arr = new char[10];
             for (int i = 0; i < Arr.Length; i++)
@@ -42,6 +43,15 @@ namespace TicTacToe
                 }
             }
             
+        }
+        private static void DisplayBoard(char[] Arr)
+        {
+            Console.WriteLine( Arr[0] +"  |  "+ Arr[1] +  "  |  " + Arr[2]);             
+            Console.WriteLine("--------------    ");
+            Console.WriteLine(Arr[3] + "  |  " + Arr[4] + "  |  " + Arr[5]);
+            Console.WriteLine("--------------    ");
+            Console.WriteLine(Arr[6] + "  |  " + Arr[7] + "  |  " + Arr[8]);
+
         }
     }
 }
